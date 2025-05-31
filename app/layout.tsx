7a414a9774e86659deca2048e0c278e51c56ea15
thebookish <abulhasannabil@gmail.com>
2025-05-31 22:23:39 +0600

    added meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,12 @@ import { Analytics } from "@vercel/analytics/react";
 const font = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Advertsine | Design & Development Studio",
+  title: "Advertsine | We Build, You Grow",
   description: "Grow your business with Advertsine, a design & development studio.",
   openGraph: {
     images: [
       {
-        url: 'https://www.google.com/maps/uv?viewerState=lb&pb=!1s0x11ae81c94242f451:0x2a6b0cac40414137!5sGlobstand+technologies&imagekey=!1e10!2sAF1QipPrTTNGxsOszQTXFD3b68e40o0V_LiVsTWwh-H6&cr=rp_35',
+        url: 'https://www.advertsine.com/images/banner.png',
         width: 1200,
         height: 630,
         alt: 'Bird Logo',
